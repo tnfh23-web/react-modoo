752f3f6ab146804ad6340415fadd1e1a7369a0ed
Lee-geon-gyu <gungyu0203@gmail.com>
2026-04-28 18:45:57 +0900

    files add

---
+++ gy/src/App.js
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import Lenis from "lenis";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+import Gyu from "./pages/Gyu";
+import Suro from "./pages/Suro";
+
+gsap.registerPlugin(ScrollTrigger);
+
+function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      smoothWheel: true,
+      lerp: 0.08,
+    });
+
+    lenis.on("scroll", ScrollTrigger.update);
+
+    const update = (time) => {
+      lenis.raf(time * 1000);
+    };
+
+    gsap.ticker.add(update);
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      gsap.ticker.remove(update);
+      lenis.destroy();
+    };
+  }, []);
+
+  return (
+    <>
+      <Gyu />
+      <Suro />
+    </>
+  );
+}
+
+export default App;
---
+++ gy/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+import "./styles/normalization.css";
+import "./styles/variable.css";
+import "lenis/dist/lenis.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
